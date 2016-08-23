145e684252abf1e3b94d043fda392e1371274c22
Halfwit <michaelmisch1985@gmail.com>
2016-08-22 22:46:47 -0600

    Iterate through possible (currently known) types until we have a value. Still doesn't output arrays of values, will have to fix later

--- watom.c
+++ watom.c
@@ -11,16 +11,18 @@
 static xcb_connection_t *conn;
 
 static void usage(char *);
-static int get_watom(xcb_window_t, char *, char *);
+static int get_watom(xcb_window_t, char *);
 
 static void
 usage(char *name)
 {
-	fprintf(stderr, "usage: %s <wid> <ATOM_NAME> <ATOM_TYPE>\n", name);
+	fprintf(stderr, "usage: %s <wid> <ATOM_NAME>\n", name);
 	exit(1);
 }
 
-static xcb_atom_t xcb_atom_get(xcb_connection_t *conn, char *name)
+const char *types[] = { "STRING", "UTF8_STRING", "COMPOUND_TEXT", "CARDINAL", "ATOM", "WM_HINTS", "WM_SIZE_HINTS"};
+
+static xcb_atom_t xcb_atom_get(xcb_connection_t *conn, const char *name)
 {
   xcb_intern_atom_cookie_t cookie = xcb_intern_atom(conn, 0, strlen(name), name);
   xcb_intern_atom_reply_t *reply = xcb_intern_atom_reply(conn, cookie, NULL);
@@ -28,33 +30,33 @@ static xcb_atom_t xcb_atom_get(xcb_connection_t *conn, char *name)
 }
 
 static int
-get_watom(xcb_window_t win, char *prop, char *type)
+get_watom(xcb_window_t win, char *prop)
 {
   int len = 0;
   xcb_get_property_cookie_t cookie;
-  xcb_get_property_reply_t *r;
   
-  cookie = xcb_get_property(conn, 0, win,
-  xcb_atom_get(conn, prop),
-  xcb_atom_get(conn, type),
-    0L, 32L);
-  r = xcb_get_property_reply(conn, cookie, NULL);
-
-  if (r) {
-    len = xcb_get_property_value_length(r);
+  size_t i;
+  for(i = 0; i < sizeof(types) / sizeof(char *); i++) {
+    cookie = xcb_get_property(conn, 0, win,
+      xcb_atom_get(conn, prop),
+      xcb_atom_get(conn, types[i]),
+      0L, 32L);
+    xcb_get_property_reply_t *r;
+    r = xcb_get_property_reply(conn, cookie, NULL);
+  
+    if (r) {
+      len = xcb_get_property_value_length(r);
     
-    if(len == 0){
-      free(r);
-      return 1;
+      if(len > 0){
+        printf("%.*s\n", len, (char *) xcb_get_property_value(r));
+        free(r);
+        return 0;
+      }
     }
-
-    printf("%.*s\n", len, (char *) xcb_get_property_value(r));
     free(r);
-    return 0;
   }
 
   warnx("could not get %s\n", prop);
-  free(r);
   return 1;
 }
 
@@ -63,12 +65,12 @@ main(int argc, char **argv)
 {
 	int r = 0;
 
-	if (argc != 4)
+	if (argc != 3)
 		usage(argv[0]);
 
 	init_xcb(&conn);
 
-	r = get_watom(strtoul(argv[1], NULL, 16), argv[2], argv[3]);
+	r = get_watom(strtoul(argv[1], NULL, 16), argv[2]);
 
 	kill_xcb(&conn);
 
